0e0fc8b804857fe3f0f9f20e4e5c9bf3ba659750
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-02-06 21:49:33 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -8,18 +8,19 @@ subprocess.run(['pip', 'install', '-Uqq', 'timm'])
 from fastai.vision.all import *
 import gradio as gr
 
-def finding(x): return x[0].isupper()
+# Define a custom label function
+def diagnosis(x): return x[0].isupper()
 
 learn = load_learner('model.pkl')
 
-#Classifies an x-ray image and returns probabilities for 'No Finding', 'Obstructive Pulmonary Disease' and 'Pneumonia'
-categories = ('No Finding', 'Obstructive Pulmonary Disease', 'Pneumonia')
+categories = ('Degenerative Infectious Disease', 'Mediastinal Anomalies', 'No Finding', 'Obstructive Pulmonary Disease', 'Pneumonia')
 
+# Function that predicts an image's category and returns a dictionary mapping categories to their probabilities
 def classify_image(img):
     pred,idx,probs = learn.predict(img)
     return dict(zip(categories, map(float,probs)))
 
-#Creates a Gradio interface for image classification
+# Create and launch a Gradio interface, allowing interactive image classification
 examples = ['normal.jpeg', 'obs.jpeg', 'pneu.jpeg']
 intf = gr.Interface(fn=classify_image, inputs='image', outputs='label', examples=examples)
 intf.launch(inline=False)
